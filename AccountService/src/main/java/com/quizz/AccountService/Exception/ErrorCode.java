a576b0825acc0cdd7949bfe784a97c5680f04232
letan-165 <2251160057@ut.edu.vn>
2025-05-13 14:01:03 +0700

    chore: fix code

--- AccountService/src/main/java/com/quizz/AccountService/Exception/ErrorCode.java
+++ AccountService/src/main/java/com/quizz/AccountService/Exception/ErrorCode.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatusCode;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    ROLE_INVALID(1003,"Lỗi vai trò người dùng (role) không hợp lệ", HttpStatus.BAD_REQUEST),
-    PASSWORD_LOGIN_FAIL(1003,"Lỗi người dùng (user) đăng nhập sai mật khẩu", HttpStatus.BAD_REQUEST),
-    USER_EXIST(1003,"Lỗi người dùng (user) đã tồn tại trong hệ thống", HttpStatus.BAD_REQUEST),
+    ROLE_INVALID(1006,"Lỗi vai trò người dùng (role) không hợp lệ", HttpStatus.BAD_REQUEST),
+    PASSWORD_LOGIN_FAIL(1005,"Lỗi người dùng (user) đăng nhập sai mật khẩu", HttpStatus.BAD_REQUEST),
+    USER_EXIST(1004,"Lỗi người dùng (user) đã tồn tại trong hệ thống", HttpStatus.BAD_REQUEST),
     USER_NO_EXIST(1003,"Lỗi không tìm thấy người dùng (user)", HttpStatus.BAD_REQUEST),
     AUTHENTICATION(1002,"Lỗi xác thực không đúng", HttpStatus.UNAUTHORIZED),
     AUTHORIZED(1001,"Lỗi không có quyền", HttpStatus.FORBIDDEN),
