5e9fb755df799d2bae864dd2f8ec6963eeb21948
letan-165 <2251160057@ut.edu.vn>
2025-05-13 13:12:19 +0700

    feat: Init ExceptionCustom

---
+++ AccountService/src/main/java/com/quizz/AccountService/Exception/AppException.java
@@ -0,0 +1,13 @@
+package com.quizz.AccountService.Exception;
+
+import lombok.Data;
+
+@Data
+public class AppException extends RuntimeException{
+    ErrorCode errorCode;
+
+    public AppException(ErrorCode e){
+        super(e.getMessage());
+        this.errorCode = e;
+    }
+}
---
+++ AccountService/src/main/java/com/quizz/AccountService/Exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.quizz.AccountService.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    ROLE_INVALID(1003,"Lỗi vai trò người dùng (role) không hợp lệ", HttpStatus.BAD_REQUEST),
+    PASSWORD_LOGIN_FAIL(1003,"Lỗi người dùng (user) đăng nhập sai mật khẩu", HttpStatus.BAD_REQUEST),
+    USER_EXIST(1003,"Lỗi người dùng (user) đã tồn tại trong hệ thống", HttpStatus.BAD_REQUEST),
+    USER_NO_EXIST(1003,"Lỗi không tìm thấy người dùng (user)", HttpStatus.BAD_REQUEST),
+    AUTHENTICATION(1002,"Lỗi xác thực không đúng", HttpStatus.UNAUTHORIZED),
+    AUTHORIZED(1001,"Lỗi không có quyền", HttpStatus.FORBIDDEN),
+    OTHER_ERROL(9999,"Lỗi mới chưa định nghĩa", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    int code;
+    String message;
+    HttpStatusCode httpStatusCode;
+
+}
